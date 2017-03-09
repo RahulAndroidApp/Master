4277e3cab7f6653c6a9aacde7b4cb9831a0c6637
sumeena <sumeena@ourdesignz.com>
2017-03-09 16:44:08 +0530

    Message goes here

--- app/src/main/java/com/example/ourdesignz/myapplication/Test.java
+++ app/src/main/java/com/example/ourdesignz/myapplication/Test.java
@@ -233,12 +233,19 @@ public class Test extends Activity
      *     which is either PERMISSION_GRANTED or PERMISSION_DENIED. Never null.
      */
 
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(
+                requestCode, permissions, grantResults, this);
+    }
 
 /**
      * Callback for when a permission is granted using the EasyPermissions
      * library.
-     * @param requestCode The request code associated with the requested
+     * @param requestCode The request code associated with the requestedgit
      *         permission
      * @param list The requested permission list. Never null.
      */
