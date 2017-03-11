b15409cc072ed230727face4c47d87e13ed02a22
sumeena <sumeena@ourdesignz.com>
2017-03-11 11:56:01 +0530

    Feature4

---
+++ app/src/main/java/com/example/ourdesignz/myapplication/Feature4.java
@@ -0,0 +1,17 @@
+package com.example.ourdesignz.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by ourdesignz on 11/3/17.
+ */
+
+public class Feature4  extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
