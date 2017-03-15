fd66cb5b1c168b42a9c67edd6f60fe9c824dd631
sumeena <sumeena@ourdesignz.com>
2017-03-15 14:23:28 +0530

    feature10

---
+++ app/src/main/java/com/example/ourdesignz/myapplication/Feature10.java
@@ -0,0 +1,17 @@
+package com.example.ourdesignz.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by ourdesignz on 11/3/17.
+ */
+
+public class Feature10 extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
